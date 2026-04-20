4634ef439b2fdcfe09070cbd13389aa8c5fb500e
Juan Francisco Llamoja <juanfrallamojajavier@gmail.com>
2026-04-19 23:54:47 -0500

    Creacion de estructura de carpetas

---
+++ src/main/java/com/jfra_13/grupos_electrogenos/GruposElectrogenosApplication.java
@@ -0,0 +1,13 @@
+package com.jfra_13.grupos_electrogenos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GruposElectrogenosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GruposElectrogenosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jfra_13/grupos_electrogenos/GruposElectrogenosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfra_13.grupos_electrogenos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruposElectrogenosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
