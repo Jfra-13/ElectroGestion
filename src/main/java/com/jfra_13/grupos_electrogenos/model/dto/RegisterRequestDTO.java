3117115b6d9302aaae9d0c8587f1878d27edbbb7
Juan Francisco Llamoja <juanfrallamojajavier@gmail.com>
2026-06-07 11:59:04 -0500

    TK009

--- src/main/java/com/jfra_13/grupos_electrogenos/model/dto/RegisterRequestDTO.java
+++ src/main/java/com/jfra_13/grupos_electrogenos/model/dto/RegisterRequestDTO.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Set;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -13,5 +11,4 @@ public class RegisterRequestDTO {
     private String username;
     private String password;
     private String email;
-    private Set<String> roles;
 }
